96f374de803ebd5658a4da511f03bf9c43f22e50
Святослав Поникаровских <tauris000@mail.ru>
2020-05-30 17:02:45 +0300

    commit RealEstate ui + code #3

--- Form1.cs
+++ Form1.cs
@@ -10,11 +10,45 @@ using System.Windows.Forms;
 
 namespace Esoft_Project
 {
-    public partial class Form1 : Form
+    public partial class Menu : Form
     {
-        public Form1()
+        public Menu()
         {
             InitializeComponent();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonPpenClients_Click(object sender, EventArgs e)
+        {
+            Form formClient = new FormClient();
+            formClient.Show();
+
+        }
+
+        private void Menu_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void buttonOpenAgents_Click(object sender, EventArgs e)
+        {
+            Form formClient = new FormAgents();
+            formClient.Show();
+        }
+
+        private void buttonOpenRealEstates_Click(object sender, EventArgs e)
+        {
+            Form formRealEstate = new FormRealEstateSet();
+            formRealEstate.Show();
         }
     }
 }
